60f95f3faf03eb469ec10ab344a3c02d574150b1
R-SMKIM-S <ksm1538@naver.com>
2022-08-24 16:45:50 +0900

    env setting but not ignore

--- src/server.js
+++ src/server.js
@@ -7,10 +7,15 @@ const {generateFakeData3} = require("../faker3");
 
 const {userRouter, boardRouter} = require('./routes');          // userRouter 불러오기
 
-const MONGO_URI = 'mongodb+srv://seongmok:3751538@mongodbstudycluster.f1v7mjy.mongodb.net/Board';
+// env환경변수를 이용해서 git에 보안 정보들이 올라가지 않게 할 수 있음.
+// 설치 : npm i env-cmd
+// 최상위 디렉터리의 .env 파일에 MONGO_URI를 정의하면 된다. (공백이 있으면 안됨)
+// .env 파일을 .gitignore 파일에 등록해두면 git에 커밋되지 않기 때문에 보안 정보를 커밋하지 않을 수 있다.
+const {MONGO_URI, PORT} = process.env;        // package.json에 env-cmd를 앞에 적어줘야함
 
 const server = async() => {
     try{
+
         await mongoose.connect(MONGO_URI);
         //mongoose.set("debug", true);        // 쿼리를 콘솔에 보이고 싶을 때
 
@@ -24,8 +29,8 @@ const server = async() => {
         app.use(express.json());        
 
         // 포트 설정
-        app.listen(3000, async () => {
-            console.log("server opened port : 3000");
+        app.listen(PORT, async () => {
+            console.log(`server opened port : ${PORT}`);
             //await generateFakeData2(10, 3, 10);  // 임시 데이터 생성 2
             //await generateFakeData3(1000000);  // 임시 데이터 생성 3
         });
